c9a60318f1e26efe0fcd0115030028008cbcae98
mohdhanif1234 <hanif.topia@gmail.com>
2022-12-11 23:56:25 +0530

    Pizza Bill Generator Assignment submitted

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -1,14 +1,27 @@
 package com.driver;
 
 public class Pizza {
-
     private int price;
     private Boolean isVeg;
     private String bill;
+    private int extraCheesePrize = 80;
+    private int extraToppingsVeg = 70;
+    private int extraToppingsNonVeg = 120;
+    private int takeAway = 20;
+    private int basePizzaPrice;
+    private boolean isExtraCheeseAdded = false;
+    private boolean isExtraToppingsAdded = false;
+    private boolean isOptedForTakeAway = false;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        // your code goes here
+        if(this.isVeg){
+            this.price = 300;
+        }
+        else{
+            this.price = 400;
+        }
+        basePizzaPrice = this.price;
     }
 
     public int getPrice(){
@@ -16,19 +29,41 @@ public class Pizza {
     }
 
     public void addExtraCheese(){
-        // your code goes here
+        isExtraCheeseAdded = true;
+        this.price += extraCheesePrize;
     }
 
     public void addExtraToppings(){
-        // your code goes here
+        isExtraToppingsAdded = true;
+        if(this.isVeg){
+            this.price += extraToppingsVeg;
+        }
+        else{
+            this.price += extraToppingsNonVeg;
+        }
     }
 
     public void addTakeaway(){
-        // your code goes here
+        isOptedForTakeAway = true;
+        this.price += takeAway;
     }
 
     public String getBill(){
-        // your code goes here
+        this.bill = "";
+        System.out.println("Base Price Of The Pizza: " + basePizzaPrice);
+        if(isExtraCheeseAdded){
+            this.bill += "Extra Cheese Added: " + extraCheesePrize + "\n";
+        }
+        if(isExtraToppingsAdded && isVeg){
+            this.bill += "Extra Toppings Added: " + extraToppingsVeg + "\n";
+        }
+        else if(isExtraToppingsAdded && !isVeg){
+            this.bill += "Extra Toppings Added: " + extraToppingsNonVeg + "\n";
+        }
+        if(isOptedForTakeAway){
+            this.bill += "Paperbag Added: " + takeAway + "\n";
+        }
+        this.bill += "Total Price: " + this.price;
         return this.bill;
     }
 }
